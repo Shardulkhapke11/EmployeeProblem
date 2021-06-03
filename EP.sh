b6bc5d049174a9241cfdfd88ece78dedf15dd97a
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-04 01:44:38 +0530

    Added: Sum of Two Numbers

--- EP.sh
+++ EP.sh
@@ -1,8 +1,13 @@
 isPresent=1
+salaryPerHr=20
+empHr=0
+totalSalary=0
 randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq $isPresent ]
 then
-echo "Employee is Present"
-else 
-echo "Employee is Absent"
+empHr=8
+else
+empHr=0
 fi
+totalSalary=$(($salaryPerHr*$empHr))
+echo "Salary of Employee is:-" $totalSalary
