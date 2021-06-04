04744326b6fcc9415df731da81a9d92bd35a3efe
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-05 00:50:15 +0530

    Added: UC3 part

--- EP.sh
+++ EP.sh
@@ -1,4 +1,5 @@
 isFullTime=1
+isPartTime=2
 salaryPerHr=20
 empHr=0
 totalSalary=0
@@ -6,6 +7,9 @@ randomNumber=$((RANDOM%3))
 if [ $randomNumber -eq $isFullTime ]
 then
 empHr=8
+elif [ $randomnumber -eq $isFullTime ]
+then
+empHr=4
 else
 empHr=0
 fi
